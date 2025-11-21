24e0e9a5091ec426c031a06cbd64823a001b5a51
silva-alexsandro <acgdas.dev@gmail.com>
2025-11-21 20:16:08 -0300

    feat: adiciona remver e copiar color

--- src/js/domain/cardManager.js
+++ src/js/domain/cardManager.js
@@ -18,17 +18,61 @@ function createElementLi(hex) {
   spanHex.style.color = textColor;
   spanHex.textContent = hex;
 
+  // Criação do botão de remover
+  const removeButton = document.createElement("button");
+  removeButton.id = "remove-color";
+  removeButton.classList.add("btn");
+  removeButton.title = "remover-cor";
+  removeButton.style.color = textColor;
+
+  removeButton.innerHTML = '<i class="ph ph-x"></i>'; // Ícone de "x" para remover
+  removeButton.addEventListener("click", () => {
+    li.remove();
+    history.execute(getCurrentPaletteState());
+  });
+
+  // Criação do botão de bloquear
+  // const lockButton = document.createElement("button");
+  // lockButton.id = "lock-color";
+  // lockButton.classList.add("btn");
+  // lockButton.title = "bloquear-cor";
+  // lockButton.style.color = textColor;
+
+  // lockButton.innerHTML = '<i class="ph ph-lock-simple-open"></i>'; // Ícone de "cadeado aberto"
+  // lockButton.addEventListener("click", () => {
+  //   const locked = li.dataset.locked === "true";
+  //   li.dataset.locked = locked ? "false" : "true"; // Alterna o estado do bloqueio
+  //   lockButton.innerHTML = locked
+  //     ? '<i class="ph ph-lock-simple-open"></i>'
+  //     : '<i class="ph ph-lock"></i>';
+  //   history.execute(getCurrentPaletteState());
+  // });
+
+  // Criação do botão de copiar
+  const copyButton = document.createElement("button");
+  copyButton.id = "copy-color";
+  copyButton.classList.add("btn");
+  copyButton.title = "copiar-cor";
+  copyButton.style.color = textColor;
+
+  copyButton.innerHTML = '<i class="ph ph-copy"></i>'; // Ícone de "copiar"
+  copyButton.addEventListener("click", () => copyColor(hex, li));
+
+  // Adicionando os botões ao <li>
+  li.appendChild(removeButton);
+  // li.appendChild(lockButton);
+  li.appendChild(copyButton);
   li.appendChild(spanHex);
+
+  // Definindo a cor de fundo do <li>
   li.style.backgroundColor = hex;
-
-  li.addEventListener("click", () => copyColor(hex, li));
-
   li.addEventListener("keypress", (event) => {
     if (event.key === "Enter" || event.key === " ") {
       event.preventDefault();
       copyColor(hex, li);
     }
   });
+
   return li;
 }
 
@@ -55,11 +99,11 @@ export function generatePalette() {
 export async function copyColor(color, card) {
   try {
     await navigator.clipboard.writeText(color);
-    showColorOKLCH(color);
     const feedback = document.createElement("div");
     feedback.classList.add("copied-message");
     feedback.textContent = "Copiado!";
     card.appendChild(feedback);
+    card.style.position = "relative";
     setTimeout(() => feedback.remove(), 2000);
   } catch (e) {
     console.error("Falha ao copiar cor:", e);
